57d12770be8789d34ecf77ffb8251217fa363f4e
oscar26 <deangel95@gmail.com>
2016-04-13 21:42:05 -0500

    Front end library including

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -1,1 +1,12 @@
+'use strict';
 
+var app = angular.module('app', ['lumx', 'ui.router']); // Change app name
+
+app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise("/");
+
+        // States go here
+
+    }
+]);
