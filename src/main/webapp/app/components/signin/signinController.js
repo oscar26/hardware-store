f45609af6b9de1e7b198d1dd3656119b6b08e381
oscar26 <deangel95@gmail.com>
2016-04-22 18:10:53 -0500

    Several aesthetic changes

--- src/main/webapp/app/components/signin/signinController.js
+++ src/main/webapp/app/components/signin/signinController.js
@@ -5,8 +5,8 @@ var signinController = angular.module('signinController', []);
 signinController.controller('signinCtrl', ['$scope',
     function ($scope) {
         $scope.user = {
-            username: "Usuario o correo electrónico",
-            password: "Contraseña"
+            username: "",
+            password: ""
         };
     }
 ]);
