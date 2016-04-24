a88c0137822a541e2269e5ae3572652aba676ef4
oscar26 <deangel95@gmail.com>
2016-04-24 14:06:44 -0500

    Product list improved aesthetics

--- src/main/webapp/app/components/productList/productListController.js
+++ src/main/webapp/app/components/productList/productListController.js
@@ -2,10 +2,32 @@
 
 var productListController = angular.module('productListController', []);
 
-productListController.controller('productListCtrl', ['$scope',
-    function ($scope) {
+productListController.controller('productListCtrl', ['$scope', '$rootScope', 'LxDialogService',
+    function ($scope, $rootScope, LxDialogService) {
+
         $scope.searchOptions = {
             term: ""
+        };
+
+        $scope.dialogOptions = {
+            dialogId: 'extra-info-dialog',
+            selectedItemId: undefined
+        };
+        
+        $scope.addToShoppingCart = function (itemId) {
+            if ($rootScope.$storage.shoppingCart == null) {
+                $rootScope.$storage.shoppingCart = new Array();
+            } else {
+                $rootScope.$storage.shoppingCart.push(itemId);
+            }
+            console.log(itemId);
+        };
+
+        $scope.openDialog = function (itemId, companyId) {
+            // Set data first
+            console.log("Opening dialog with itemId-companyId: ", itemId, companyId);
+            $scope.dialogOptions.selectedItemId = itemId;
+            LxDialogService.open($scope.dialogOptions.dialogId);
         };
 
         $scope.$watch(
