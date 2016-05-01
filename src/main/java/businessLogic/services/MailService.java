f7db96915e4f823f17b09acadc94af1d5d8ec263
oscar26 <deangel95@gmail.com>
2016-04-30 19:25:16 -0500

    Revert "Added files via upload"
    This reverts commit 9848be7a883eaab4935ce234c425c5913434620a.

--- src/main/java/businessLogic/services/MailService.java
+++
@@ -1,7 +0,0 @@
-package businessLogic.services;
-
-/**
- * Created by Gerardth on 29/04/2016.
- */
-public class MailService { // metodo que reciba el cuerpo, el archivo y el correo destino, el asunto
-}
